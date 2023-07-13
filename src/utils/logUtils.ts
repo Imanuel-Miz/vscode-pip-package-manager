b9f37bbdf65f682a427fef37acc0d45b6d3c8fce
imanuel-miz <imanuel.mizrahi@rightbound.io>
2023-07-13 12:33:18 +0300

    log handling

--- src/utils/logUtils.ts
+++ src/utils/logUtils.ts
@@ -1,20 +1,11 @@
 import { window } from 'vscode';
-
-const outputChannel = window.createOutputChannel('pip-package-manager-outputs');
-const { Color } = require('chalk');
-const timeColor = new Color('green')
+const outputChannel = window.createOutputChannel('pip-package-manager-outputs', 'typescript');
 
 export enum logType {
     ERROR = 'ERROR',
     WARNING = 'WARNING',
     INFO = 'INFO'
 }
-
-const logColorMap = {
-    [logType.ERROR]: new Color('red'),
-    [logType.WARNING]: new Color('yellow'),
-    [logType.INFO]: new Color('blue')
-};
 
 function padTo2Digits(num: number) {
     return num.toString().padStart(2, '0');
@@ -36,17 +27,8 @@ function formatDate(date: Date) {
     );
 }
 
-function _logWithHighlightedError(text: string) {
-    const regex = /(error)/gi;
-    const coloredText = text.replace(regex, (match, capture) => logColorMap[logType.ERROR](capture));
-    return coloredText
-}
-
 export function sendOutputLogToChannel(logMessage: string, logType: logType) {
-    const timeLog = timeColor(`[${formatDate(new Date())}]`)
-    const logTypeColor = logColorMap[logType];
-    const formattedLogType = logTypeColor(`[${logType}]`)
-    const formattedMessage = `${timeLog} ${formattedLogType} ${_logWithHighlightedError(logMessage)}`;
+    const formattedMessage = `[${formatDate(new Date())}] [${logType}] ${logMessage}`;
     outputChannel.appendLine(formattedMessage)
     outputChannel.show()
 }
